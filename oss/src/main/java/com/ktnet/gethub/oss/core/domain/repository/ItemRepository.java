af794150764cdf79aba7bafafef38dd8e9f192ef
EHOTO <ktaewoong46@gmail.com>
2020-08-21 14:52:14 +0900

    상품 등록 기능 구현 완료

--- oss/src/main/java/com/ktnet/gethub/oss/core/domain/repository/ItemRepository.java
+++ oss/src/main/java/com/ktnet/gethub/oss/core/domain/repository/ItemRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository // @Repository가 존재하는 클래스는 Spring Bean 객체로 등록을 해준다!
 @RequiredArgsConstructor
@@ -14,9 +15,9 @@ public class ItemRepository {
 
     public void save(Item item) {
         /**
-         * 완전 신규 생성 시, ID 값이 NULL 이다.
-         * 이 경우, em.persist를 통해서 새로 생성하는 것이고
-         * 아닐 경우, em.merge를 통해서...
+         * 파라미터 들어온 상품의 ID 값에 따라 다른 프로세를 돈다 !!!
+         * NULL인 경우, em.persist를 통해서 상품을 새로 생성하는 것이고,
+         * NOT NULL인 경우, em.merge를 통해서 강제로 업데이트(?)하는 것이다 !!!
          */
         if(item.getId() == null) {
             em.persist(item);
@@ -25,5 +26,13 @@ public class ItemRepository {
         }
     }
 
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
 
 }
---
+++ oss/src/main/java/com/ktnet/gethub/oss/core/service/ItemService.java
@@ -0,0 +1,35 @@
+package com.ktnet.gethub.oss.core.service;
+
+import com.ktnet.gethub.oss.core.domain.dao.item.Item;
+import com.ktnet.gethub.oss.core.domain.repository.ItemRepository;
+import com.ktnet.gethub.oss.core.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public Item findItem(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+}
+/**
+ * Repository 클래스로 위임만 하는 역할이 서비스라고 생각하면 된다!
+ */
